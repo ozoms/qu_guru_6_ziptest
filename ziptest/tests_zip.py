6994ffd2e5f7d25e1cd108d95544332a4685d588
sasha <slod80@gmail.com>
2022-12-13 00:03:06 +0600

    First commit

---
+++ ziptest/tests_zip.py
@@ -0,0 +1,23 @@
+#from zipfile import ZipFile
+import pathlib
+import zipfile
+import os
+import shutil
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+resources_dir = os.path.join(current_dir, 'resources')
+directory = pathlib.Path(resources_dir)
+
+with zipfile.ZipFile('testzip.zip', mode="w") as archive:
+    for file_path in directory.iterdir():
+        archive.write(file_path, arcname=file_path.name)
+
+resources_zip_dir = os.path.join(resources_dir, 'testzip.zip')
+directory_testzip = pathlib.Path(resources_zip_dir)
+
+shutil.move('testzip.zip', directory)
+
+with zipfile.ZipFile(directory_testzip, mode="r") as archive:
+    name_info = archive.namelist()
+
+assert ['tests_data.csv', 'tests_doc.pdf', 'tests_table.xlsx'] == name_info
