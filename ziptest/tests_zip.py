d6459e38666f303e3f62e263b741d88aaaeabaf3
sasha <slod80@gmail.com>
2023-01-08 19:06:11 +0600

    Добавлены: удаление временных файлов и pytest.

--- ziptest/tests_zip.py
+++ ziptest/tests_zip.py
@@ -6,27 +6,34 @@ import csv
 from PyPDF2 import PdfReader
 from openpyxl import load_workbook
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
-resources_dir = os.path.join(current_dir, '../resources')
-directory = pathlib.Path(resources_dir)
 
-with zipfile.ZipFile('testzip.zip', mode="w") as archive:
-    for file_path in directory.iterdir():
-        archive.write(file_path, arcname=file_path.name)
+def test_zipfile():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    resources_dir = os.path.join(current_dir, '../resources')
+    directory = pathlib.Path(resources_dir)
 
-shutil.move('testzip.zip', directory)
+    with zipfile.ZipFile('testzip.zip', mode="w") as archive:
+        for file_path in directory.iterdir():
+            archive.write(file_path, arcname=file_path.name)
 
-resources_zip_dir = os.path.join(resources_dir, 'testzip.zip')
+    shutil.move('testzip.zip', directory)
 
-zipfile.ZipFile(resources_zip_dir).extractall()
+    resources_zip_dir = os.path.join(resources_dir, 'testzip.zip')
 
-text = PdfReader('tests_doc.pdf').pages[0].extract_text()
-assert '1 a' in text
+    zipfile.ZipFile(resources_zip_dir).extractall()
 
-with open('tests_data.csv') as csvfile:
-    csvfile = csv.reader(csvfile)
-    assert ['1', 'a'] in csvfile
+    text = PdfReader('tests_doc.pdf').pages[0].extract_text()
+    assert '1 a' in text
 
-workbook = load_workbook('tests_table.xlsx')
-sheet = workbook.active
-assert 'a' == sheet.cell(row=1, column=2).value
+    with open('tests_data.csv') as csvfile:
+        csvfile = csv.reader(csvfile)
+        assert ['1', 'a'] in csvfile
+
+    workbook = load_workbook('tests_table.xlsx')
+    sheet = workbook.active
+    assert 'a' == sheet.cell(row=1, column=2).value
+
+    os.remove(resources_zip_dir)
+    os.remove('tests_doc.pdf')
+    os.remove('tests_data.csv')
+    os.remove('tests_table.xlsx')
